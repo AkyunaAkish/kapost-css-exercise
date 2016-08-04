97cc9ff983caee76dab70622311ba048a59787e8
AkyunaAkish <akyunaakish@gmail.com>
2016-08-04 11:05:39 -0600

    Added Button to Switch Between versions of the tool bar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,10 +3,36 @@ import ToolBar from './ToolBar';
 import Grid from './Grid';
 
 class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toolbarVersion: 1
+    }
+  }
+
+  handleToolBarSwitch() {
+    switch (this.state.toolbarVersion) {
+      case 1:
+      this.setState({
+        toolbarVersion: 2
+      })
+      break;
+      case 2:
+      this.setState({
+        toolbarVersion: 1
+      })
+      break;
+    }
+  }
+
   render() {
     return (
       <div>
-        <ToolBar />
+        <button id='toolBarSwitcher' onClick={this.handleToolBarSwitch.bind(this)}>
+          Switch Tool Bar Version
+        </button>
+
+        {this.state.toolbarVersion === 1 ? <ToolBar version={1}/> : <ToolBar version={2}/>}
         <Grid />
       </div>
     )
--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -1,24 +1,57 @@
 import React, { Component } from 'react';
 
 class ToolBar extends Component {
+
+  getToolBarVersion(versionId) {
+    switch (versionId) {
+      case 1:
+      return (
+        <nav id='toolBar1'>
+          <button id='addNewButton1'>Add New</button>
+
+          <select id='bulkActionsSelect1'>
+            <option value='Bulk Actions'>Bulk Actions</option>
+          </select>
+
+          <div id='toolBarSearch1'>
+            <p>Count per page: <span>10 items</span></p>
+            <input type='text' placeholder='Search'/>
+          </div>
+
+          <button id='expandToolBarButton1'></button>
+
+          <button id='funnelButton1'></button>
+        </nav>
+      )
+      break;
+      case 2:
+      return (
+        <nav id='toolBar2'>
+          <button id='addNewButton2'>Add New</button>
+
+          <select id='bulkActionsSelect2'>
+            <option value='Bulk Actions'>Bulk Actions</option>
+          </select>
+
+          <div id='toolBarSearch2'>
+            <p>Count per page: <span>10 items</span></p>
+            <input type='text' placeholder='Search'/>
+          </div>
+
+          <button id='expandToolBarButton2'></button>
+
+          <button id='funnelButton2'></button>
+        </nav>
+      )
+      break;
+    }
+  }
+
   render() {
     return (
-      <nav id='toolBar'>
-        <button id='addNewButton'>Add New</button>
-
-        <select id='bulkActionsSelect'>
-          <option value='Bulk Actions'>Bulk Actions</option>
-        </select>
-
-        <div id='toolBarSearch'>
-          <p>Count per page: <span>10 items</span></p>
-          <input type='text' placeholder='Search'/>
-        </div>
-
-        <button id='expandToolBarButton'></button>
-
-        <button id='funnelButton'></button>
-      </nav>
+      <span>
+        {this.getToolBarVersion(this.props.version)}
+      </span>
     )
   }
 };
